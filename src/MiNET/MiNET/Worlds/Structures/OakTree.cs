8ad69bcc2a2482b6dbf0ec595dc703c3d831af32
kennyvv <kennyvvulpen@gmail.com>
2015-02-22 21:14:47 +0100

    Added OakTree Structure
    Sorry for the unneeded commits. Something went wrong :|

---
+++ src/MiNET/MiNET/Worlds/Structures/OakTree.cs
@@ -0,0 +1,42 @@
+﻿using MiNET.Blocks;
+using MiNET.Utils;
+
+namespace MiNET.Worlds.Structures
+{
+	class OakTree : Structure
+	{
+		public override string Name
+		{
+			get { return "OakTree"; }
+		}
+
+		public override Block[] Blocks
+		{
+			get
+			{
+				return new Block[]
+				{
+					new Block(17) {Coordinates = new BlockCoordinates(0, 0, 0)},
+					new Block(17) {Coordinates = new BlockCoordinates(0, 1, 0)},
+					new Block(17) {Coordinates = new BlockCoordinates(0, 2, 0)},
+					new Block(17) {Coordinates = new BlockCoordinates(0, 3, 0)},
+					new Block(17) {Coordinates = new BlockCoordinates(0, 4, 0)},
+					new Block(17) {Coordinates = new BlockCoordinates(0, 5, 0)},
+					new Block(18) {Coordinates = new BlockCoordinates(-1, 4, 1)},
+					new Block(18) {Coordinates = new BlockCoordinates(1, 4, -1)},
+					new Block(18) {Coordinates = new BlockCoordinates(-1, 4, -1)},
+					new Block(18) {Coordinates = new BlockCoordinates(1, 4, 1)},
+					new Block(18) {Coordinates = new BlockCoordinates(-1, 4, 0)},
+					new Block(18) {Coordinates = new BlockCoordinates(1, 4, 0)},
+					new Block(18) {Coordinates = new BlockCoordinates(0, 4, -1)},
+					new Block(18) {Coordinates = new BlockCoordinates(0, 4, 1)},
+					new Block(18) {Coordinates = new BlockCoordinates(-1, 5, 0)},
+					new Block(18) {Coordinates = new BlockCoordinates(1, 5, 0)},
+					new Block(18) {Coordinates = new BlockCoordinates(0, 5, -1)},
+					new Block(18) {Coordinates = new BlockCoordinates(0, 5, 1)},
+					new Block(18) {Coordinates = new BlockCoordinates(0, 6, 0)},
+				};
+			}
+		}
+	}
+}

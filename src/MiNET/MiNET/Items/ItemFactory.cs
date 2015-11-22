aac86e06adafd45bfe7558f2e38e9d7f39eae424
Vladik46 <twwkmir@gmail.com>
2015-11-22 13:18:06 +0300

    Update ItemFactory.cs

--- src/MiNET/MiNET/Items/ItemFactory.cs
+++ src/MiNET/MiNET/Items/ItemFactory.cs
@@ -37,7 +37,7 @@ namespace MiNET.Items
 			else if (id == 44) item = new ItemSlab(id, metadata);
 			else if (id == 61) item = new ItemFurnace(metadata);
 			else if (id == 63) item = new ItemSign(metadata);
-+			else if (id == 68) item = new ItemSign(metadata);
+			else if (id == 68) item = new ItemSign(metadata);
 			else if (id == 158) item = new ItemSlab(id, metadata);
 			else if (id == 256) item = new ItemIronShovel(metadata);
 			else if (id == 258) item = new ItemIronAxe(metadata);

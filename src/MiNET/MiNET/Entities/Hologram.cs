8e3e7568edfe82f45a3a795a2b59d1baab2b0ea3
AppPhil <androidappphil@gmail.com>
2016-01-07 17:38:08 +0100

    Added Hologram to entites.
    Hologram is a floating text (simply an entity with metadata[15]=1 with
    nametag as text)

---
+++ src/MiNET/MiNET/Entities/Hologram.cs
@@ -0,0 +1,29 @@
+﻿using MiNET.Utils;
+using MiNET.Worlds;
+
+namespace MiNET.Entities
+{
+    public class Hologram : Entity
+    {
+
+        public Hologram(Level level, string text, Vector3 position) : base(64, level)
+        {
+            NameTag = text;
+            KnownPosition = new PlayerLocation(position);
+        }
+
+        public override MetadataDictionary GetMetadata()
+        {
+            var metadata = base.GetMetadata();
+            metadata[15] = new MetadataByte(1);
+            return metadata;
+        }
+
+        public override void OnTick()
+        {
+
+        }
+
+
+    }
+}

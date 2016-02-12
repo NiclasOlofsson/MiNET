de671a896499558e95795b36e3ee6db12253a1f3
SparklingSpaceCat <a.quant@mail.ru>
2016-02-12 18:16:59 +0300

    Gravel now can drop a flint

--- src/MiNET/MiNET/Blocks/Gravel.cs
+++ src/MiNET/MiNET/Blocks/Gravel.cs
@@ -3,20 +3,20 @@ using MiNET.Utils;
 
 namespace MiNET.Blocks
 {
-	public class Gravel : Block
-	{
-		public Gravel() : base(13)
-		{
-		}
+    public class Gravel : Block
+    {
+        public Gravel() : base(13)
+        {
+        }
 
-	    public override ItemStack GetDrops()
-	    {
-	        Random random = new Random();
-	        if (random.Next(10) == 0)
+        public override ItemStack GetDrops()
+        {
+            Random random = new Random();
+            if (random.Next(10) == 0)
             {
-	            return new ItemStack(318, 1);
-	        }
+                return new ItemStack(318, 1);
+            }
             return new ItemStack(13, 0);
-	    }
-	}
+        }
+    }
 }

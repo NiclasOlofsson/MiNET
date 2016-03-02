d45051fb661515b4103574b7aa4c5fd5cefc6816
NiclasOlofsson <nicke.olofsson@gmail.com>
2016-03-02 01:58:58 +0100

    Added all the mobs. Not yet useful.

--- src/MiNET/MiNET/Entities/Mob.cs
+++ src/MiNET/MiNET/Entities/Mob.cs
@@ -76,9 +76,8 @@ namespace MiNET.Entities
 	{
 		public Cow(Level level) : base(MobTypes.Cow, level)
 		{
-			Width = 0.3;
-			Length = 0.9;
-			Height = 1.3; // baby = 0.65
+			Width = Height = 0.9;
+			Height = 1.4;
 		}
 	}
 
@@ -86,9 +85,8 @@ namespace MiNET.Entities
 	{
 		public Pig(Level level) : base(MobTypes.Pig, level)
 		{
-			Width = 0.3;
-			Length = 0.9;
-			Height = 0.875; // baby = ?
+			Width = Height = 0.9;
+			Height = 0.9;
 		}
 	}
 
@@ -96,9 +94,8 @@ namespace MiNET.Entities
 	{
 		public Sheep(Level level) : base(MobTypes.Sheep, level)
 		{
-			Width = 0.625;
-			Length = 1.4375;
-			Height = 1.25; // baby = ?
+			Width = Height = 0.9;
+			Height = 1.3;
 		}
 	}
 
@@ -106,9 +103,53 @@ namespace MiNET.Entities
 	{
 		public Wolf(Level level) : base(MobTypes.Wolf, level)
 		{
-			Width = 0.3;
-			Length = 0.9;
-			Height = 0.85; // baby = 0.425
+			Width = Height = 0.6;
+			Height = 0.8;
+		}
+	}
+
+	public class Villager : Mob
+	{
+		public Villager(Level level) : base(MobTypes.Villager, level)
+		{
+			Width = Height = 0.6;
+			Height = 1.8;
+		}
+	}
+
+	public class MushroomCow : Mob
+	{
+		public MushroomCow(Level level) : base(MobTypes.MushroomCow, level)
+		{
+			Width = Height = 0.9;
+			Height = 1.4;
+		}
+	}
+
+	public class Squid : Mob
+	{
+		public Squid(Level level) : base(MobTypes.Squid, level)
+		{
+			Width = Height = 0.95;
+			Height = 0.95;
+		}
+	}
+
+	public class Rabbit : Mob
+	{
+		public Rabbit(Level level) : base(MobTypes.Rabbit, level)
+		{
+			Width = Height = 0.6;
+			Height = 0.7;
+		}
+	}
+
+	public class Bat : Mob
+	{
+		public Bat(Level level) : base(MobTypes.Bat, level)
+		{
+			Width = Height = 0.5;
+			Height = 0.9;
 		}
 	}
 
@@ -116,6 +157,152 @@ namespace MiNET.Entities
 	{
 		public IronGolem(Level level) : base(MobTypes.IronGolem, level)
 		{
+			Width = Height = 1.4;
+			Height = 2.9;
+		}
+	}
+
+	public class Snowman : Mob
+	{
+		public Snowman(Level level) : base(MobTypes.Snowman, level)
+		{
+			Width = Height = 0.7;
+			Height = 1.9;
+		}
+	}
+
+	public class Ocelot : Mob
+	{
+		public Ocelot(Level level) : base(MobTypes.Ocelot, level)
+		{
+			Width = Height = 0.6;
+			Height = 0.8;
+		}
+	}
+
+	public class Zombie : Mob
+	{
+		public Zombie(Level level) : base(MobTypes.Zombie, level)
+		{
+			Width = Height = 0.6;
+			Height = 1.8;
+		}
+	}
+
+	public class Creeper : Mob
+	{
+		public Creeper(Level level) : base(MobTypes.Creeper, level)
+		{
+			Width = Height = 0.6;
+			Height = 1.8;
+		}
+	}
+
+	public class Skeleton : Mob
+	{
+		public Skeleton(Level level) : base(MobTypes.Skeleton, level)
+		{
+			Width = Height = 0.6;
+			Height = 1.95;
+		}
+	}
+
+	public class Spider : Mob
+	{
+		public Spider(Level level) : base(MobTypes.Spider, level)
+		{
+			Width = Height = 1.4;
+			Height = 0.9;
+		}
+	}
+
+	public class ZombiePigman : Mob
+	{
+		public ZombiePigman(Level level) : base(MobTypes.ZombiePigman, level)
+		{
+			Width = Height = 0.6;
+			Height = 1.8;
+		}
+	}
+
+	public class Slime : Mob
+	{
+		public Slime(Level level) : base(MobTypes.Slime, level)
+		{
+			Width = Height = 0.51000005;
+			Height = 0.51000005;
+		}
+	}
+
+	public class Enderman : Mob
+	{
+		public Enderman(Level level) : base(MobTypes.Enderman, level)
+		{
+			Width = Height = 0.6;
+			Height = 2.9;
+		}
+	}
+
+	public class Silverfish : Mob
+	{
+		public Silverfish(Level level) : base(MobTypes.Silverfish, level)
+		{
+			Width = Height = 0.4;
+			Height = 0.3;
+		}
+	}
+
+	public class CaveSpider : Mob
+	{
+		public CaveSpider(Level level) : base(MobTypes.CaveSpider, level)
+		{
+			Width = Height = 0.7;
+			Height = 0.5;
+		}
+	}
+
+	public class Ghast : Mob
+	{
+		public Ghast(Level level) : base(MobTypes.Ghast, level)
+		{
+			Width = Height = 4.0;
+			Height = 4.0;
+		}
+	}
+
+	public class MagmaCube : Mob
+	{
+		public MagmaCube(Level level) : base(MobTypes.MagmaCube, level)
+		{
+			Width = Height = 0.51000005;
+			Height = 0.51000005;
+		}
+	}
+
+	public class Blaze : Mob
+	{
+		public Blaze(Level level) : base(MobTypes.Blaze, level)
+		{
+			Width = Height = 0.6;
+			Height = 1.8;
+		}
+	}
+
+	public class ZombieVillager : Mob
+	{
+		public ZombieVillager(Level level) : base(MobTypes.ZombieVillager, level)
+		{
+			Width = Height = 0.6;
+			Height = 1.8;
+		}
+	}
+
+	public class Witch : Mob
+	{
+		public Witch(Level level) : base(MobTypes.Witch, level)
+		{
+			Width = Height = 0.6;
+			Height = 1.8;
 		}
 	}
 }

e1a514ff7ead64d4f7c02ff404080d28bb9a73aa
Sam H <samueljh2001@gmail.com>
2016-03-26 11:54:30 +0000

    Update TextUtils.cs
    Added RemoveFormatting() method

--- src/MiNET/MiNET/Utils/TextUtils.cs
+++ src/MiNET/MiNET/Utils/TextUtils.cs
@@ -135,13 +135,24 @@ namespace MiNET.Utils
 			if (keepBold)
 			{
 				Regex rgx = new Regex("(?:&|§|\u00a7)([0123456789abcdefkmnor])");
-				result = rgx.Replace(input, "");
+ -				result = rgx.Replace(input, "\u1236");
+ -				Regex rgxBold = new Regex("(?:&|§|\u00a7)([l])");
+ -				result = rgxBold.Replace(result, "\u1234");
 			}
 			else
 			{
 				Regex rgx = new Regex("(?:&|§|\u00a7)([0123456789abcdefklmnor])");
-				result = rgx.Replace(input, "");
+				result = rgx.Replace(input, "/1236");
 			}
+			return result;
+		}
+		
+		public static string RemoveFormatting(string input)
+		{
+			string result;
+			
+			Regex rgx = new Regex("(?:&|§|\u00a7)([0123456789abcdefklmnor])");
+			result = rgx.Replace(input, "");
 			return result;
 		}
 	}

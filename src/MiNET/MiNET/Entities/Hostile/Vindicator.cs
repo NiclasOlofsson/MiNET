a5b032200a7abc0f18bd57e382df32ef89673cf0
Maksim Sunduk <undrfined@gmail.com>
2017-06-19 00:26:16 +0300

    Some fixes and added new entities (#252)
    * Added some entities, fixed lightning bolt
    
    * Fixed water damage, fixed query version, some small fixes.
    
    * Revert "Fixed water damage, fixed query version, some small fixes."
    
    This reverts commit 085e662640ac09b0dced39cfdd6252cdcc8c3509.
    
    * Another try to fix :D

---
+++ src/MiNET/MiNET/Entities/Hostile/Vindicator.cs
@@ -0,0 +1,15 @@
+﻿using MiNET.Worlds;
+
+namespace MiNET.Entities.Hostile
+{
+	public class Vindicator : HostileMob
+	{
+		public Vindicator(Level level) : base(EntityType.Vindicator, level)
+		{
+			Width = Length = 0.6;
+			Height = 1.95;
+			HealthManager.MaxHealth = 240;
+			HealthManager.ResetHealth();
+		}
+	}
+}

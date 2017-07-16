8b58bed0bf1a3a78a07308f21a6c8b35fd491b1c
NiclasOlofsson <nicke.olofsson@gmail.com>
2017-07-16 15:45:34 +0200

    Don't allow planting flowers on anything but dirt and grass.

--- src/MiNET/MiNET/Blocks/DoublePlant.cs
+++ src/MiNET/MiNET/Blocks/DoublePlant.cs
@@ -16,6 +16,17 @@ namespace MiNET.Blocks
 			IsTransparent = true;
 		}
 
+		protected override bool CanPlace(Level world, BlockCoordinates blockCoordinates, BlockFace face)
+		{
+			if (base.CanPlace(world, blockCoordinates, face))
+			{
+				Block under = world.GetBlock(Coordinates + BlockCoordinates.Down);
+				return under is Grass || under is Dirt;
+			}
+
+			return false;
+		}
+
 		public override void BlockUpdate(Level level, BlockCoordinates blockCoordinates)
 		{
 			if (Coordinates + BlockCoordinates.Down == blockCoordinates)

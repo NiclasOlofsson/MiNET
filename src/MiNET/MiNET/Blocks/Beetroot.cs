41629a48a603fddbc01f690f0fac9247a911559b
NiclasOlofsson <nicke.olofsson@gmail.com>
2017-07-16 17:46:45 +0200

    Bone meal for crops. Fixed transaprency for sky light on some blocks (PE specific).

--- src/MiNET/MiNET/Blocks/Beetroot.cs
+++ src/MiNET/MiNET/Blocks/Beetroot.cs
@@ -1,5 +1,33 @@
-﻿using System;
+﻿#region LICENSE
+
+// The contents of this file are subject to the Common Public Attribution
+// License Version 1.0. (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// https://github.com/NiclasOlofsson/MiNET/blob/master/LICENSE. 
+// The License is based on the Mozilla Public License Version 1.1, but Sections 14 
+// and 15 have been added to cover use of software over a computer network and 
+// provide for limited attribution for the Original Developer. In addition, Exhibit A has 
+// been modified to be consistent with Exhibit B.
+// 
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+// the specific language governing rights and limitations under the License.
+// 
+// The Original Code is Niclas Olofsson.
+// 
+// The Original Developer is the Initial Developer.  The Initial Developer of
+// the Original Code is Niclas Olofsson.
+// 
+// All portions of the code written by Niclas Olofsson are Copyright (c) 2014-2017 Niclas Olofsson. 
+// All Rights Reserved.
+
+#endregion
+
+using System;
+using System.Numerics;
 using MiNET.Items;
+using MiNET.Utils;
+using MiNET.Worlds;
 
 namespace MiNET.Blocks
 {
@@ -9,6 +37,21 @@ namespace MiNET.Blocks
 		{
 			MaxGrowth = 4;
 		}
+
+		public override bool Interact(Level level, Player player, BlockCoordinates blockCoordinates, BlockFace face, Vector3 faceCoord)
+		{
+			var itemInHand = player.Inventory.GetItemInHand();
+			if (Metadata < MaxGrowth && itemInHand is ItemDye && itemInHand.Metadata == 15 && new Random().NextDouble() > 0.25)
+			{
+				Metadata++;
+				level.SetBlock(this);
+
+				return true;
+			}
+
+			return false;
+		}
+
 
 		public override Item[] GetDrops(Item tool)
 		{

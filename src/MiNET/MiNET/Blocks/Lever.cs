0defd128001dae58cb105d8fbbe4375aef7cdd33
Kenny van Vulpen <kennyvvulpen@gmail.com>
2017-07-22 22:37:50 +0200

    Add Dispenser, Piston, Snowlayer & lever placement (And lever toggling :P)

--- src/MiNET/MiNET/Blocks/Lever.cs
+++ src/MiNET/MiNET/Blocks/Lever.cs
@@ -1,4 +1,8 @@
-﻿namespace MiNET.Blocks
+﻿using System.Numerics;
+using MiNET.Utils;
+using MiNET.Worlds;
+
+namespace MiNET.Blocks
 {
 	public class Lever : Block
 	{
@@ -8,6 +12,75 @@
 			IsSolid = false;
 			BlastResistance = 2.5f;
 			Hardness = 0.5f;
+		}
+
+		public override bool PlaceBlock(Level world, Player player, BlockCoordinates blockCoordinates, BlockFace face, Vector3 faceCoords)
+		{
+			var playerRotation = player.GetDirection();
+
+			if (face == BlockFace.Up)
+			{
+				switch (playerRotation)
+				{
+					case 0: //South
+						Metadata = 6;
+						break;
+					case 1: //West 
+						Metadata = 5;
+						break;
+					case 2: //North
+						Metadata = 6;
+						break;
+					case 3: //East
+						Metadata = 5;
+						break;
+				}
+			}
+			else if (face == BlockFace.Down)
+			{
+				switch (playerRotation)
+				{
+					case 0: //South
+						Metadata = 0;
+						break;
+					case 1: //West 
+						Metadata = 7;
+						break;
+					case 2: //North
+						Metadata = 0;
+						break;
+					case 3: //East
+						Metadata = 7;
+						break;
+				}
+			}
+			else if (face == BlockFace.East)
+			{
+				Metadata = 4;
+			}
+			else if (face == BlockFace.South)
+			{
+				Metadata = 1;
+			}
+			else if (face == BlockFace.North)
+			{
+				Metadata = 2;
+			}
+			else if (face == BlockFace.West)
+			{
+				Metadata = 3;
+			}
+
+			world.SetBlock(this);
+			return true;
+		}
+
+		public override bool Interact(Level world, Player player, BlockCoordinates blockCoordinates, BlockFace face, Vector3 faceCoord)
+		{
+			Metadata = BitHelper.ToggleBit(Metadata, 8);
+			world.SetBlock(this);
+
+			return true;
 		}
 	}
 }

54ef4b1f4f8b615b3336de1ea965806337e1764d
Niclas Olofsson <nicke.olofsson@gmail.com>
2018-07-07 01:35:58 +0200

    Fix NPE for clone.

--- src/MiNET/MiNET/EduTokenManager.cs
+++ src/MiNET/MiNET/EduTokenManager.cs
@@ -1,3 +1,28 @@
+#region LICENSE
+
+// The contents of this file are subject to the Common Public Attribution
+// License Version 1.0. (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// https://github.com/NiclasOlofsson/MiNET/blob/master/LICENSE. 
+// The License is based on the Mozilla Public License Version 1.1, but Sections 14 
+// and 15 have been added to cover use of software over a computer network and 
+// provide for limited attribution for the Original Developer. In addition, Exhibit A has 
+// been modified to be consistent with Exhibit B.
+// 
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+// the specific language governing rights and limitations under the License.
+// 
+// The Original Code is MiNET.
+// 
+// The Original Developer is the Initial Developer.  The Initial Developer of
+// the Original Code is Niclas Olofsson.
+// 
+// All portions of the code written by Niclas Olofsson are Copyright (c) 2014-2018 Niclas Olofsson. 
+// All Rights Reserved.
+
+#endregion
+
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -5,7 +30,6 @@ using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
 using log4net;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using MiNET.Utils;
 using Newtonsoft.Json.Linq;
 
@@ -54,7 +78,6 @@ namespace MiNET
 
 		public string FetchNewSignedToken(string tenantId, string username, string password)
 		{
-
 			/**
 			 * MUST FILL IN WITH YOUR STUFF
 			 */
@@ -108,10 +131,10 @@ namespace MiNET
 
 			string content = $@"
 			grant_type = password
-			&resource ={ resourceUri }
-			&username ={ username }
-			&password ={ password }
-			&client_id ={ clientId }";
+			&resource ={resourceUri}
+			&username ={username}
+			&password ={password}
+			&client_id ={clientId}";
 
 			message.Content = new StringContent(content, Encoding.UTF8, "application/x-www-form-urlencoded");
 
@@ -132,7 +155,7 @@ namespace MiNET
 
 			string content = $@"
 			grant_type = refresh_token
-			&refresh_token ={ refreshToken }
+			&refresh_token ={refreshToken}
 			&scope = openid";
 
 			message.Content = new StringContent(content, Encoding.UTF8, "application/x-www-form-urlencoded");
@@ -144,6 +167,5 @@ namespace MiNET
 
 			return payload.id_token.ToString();
 		}
-
 	}
 }

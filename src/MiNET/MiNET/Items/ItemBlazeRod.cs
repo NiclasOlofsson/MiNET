c82dafabbcaa1b491904cd19dc151c210d3a7108
Kaooot <jonapapenfuss@gmail.com>
2018-08-26 19:15:17 +0200

    Add BlazeRod Item and BlazePowder Item

---
+++ src/MiNET/MiNET/Items/ItemBlazeRod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiNET.Items
+{
+    public class ItemBlazeRod : Item
+    {
+        public ItemBlazeRod() : base(369)
+        {
+            MaxStackSize = 64;
+        }
+    }
+}

d98fc2d33a8bfa33ae095ef95fcc21c8cb645eb7
Алексей <lashajahsnal@gmail.com>
2021-04-28 22:34:23 +0300

    Item pick up.

--- src/MiNET/MiNET/Player.cs
+++ src/MiNET/MiNET/Player.cs
@@ -2599,6 +2599,11 @@ namespace MiNET
 			return itemEntity;
 		}
 
+		public virtual bool PickUpItem(ItemEntity item)
+		{
+			return Inventory.SetFirstEmptySlot(item.Item, true);
+		}
+
 		private bool VerifyRecipe(List<Item> craftingInput, Item result)
 		{
 			Log.Debug($"Looking for matching recipes with the result {result}");
